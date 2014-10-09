97fe75b60cb37f9ba5d8b7161402bde0662c81b3
diutsu <sousago@gmail.com>
2014-10-09 10:22:19 +0100

    Checks arguments, adds get all and remove item endpoints

--- db/models/item.js
+++ db/models/item.js
@@ -9,8 +9,15 @@ itemSchema.statics.number = function (typeS, cb) {
   this.count({type : typeS},cb);
 };
 
+itemSchema.statics.countWithId = function (itemId, cb) {
+  this.count({_id : itemId},cb);
+}
 itemSchema.statics.findAll = function (cb) {
   this.find({}, cb);
 };
+
+itemSchema.statics.remv = function(itemId,cb){
+  this.remove({_id:itemId},cb);
+}
 
 var item = module.exports = mongoose.model('item', itemSchema);
--- index.js
+++ index.js
@@ -3,56 +3,111 @@ var Hapi = require("hapi");
 var server = new Hapi.Server(8001, "localhost");
 require("./db");
 
+var alphanumExp = /^[a-z0-9]+$/i;
 server.start(function(){
-	console.log("Hapi server started @", server.info.uri);
+  console.log("Hapi server started @", server.info.uri);
 });
 
 
 
 server.route({
-    path: "/{path*}",
-    method: "GET",
-    handler: {
-        directory: {
-            path: "./public",
-            listing: false,
-            index: false
-        }
+  path: "/{path*}",
+  method: "GET",
+  handler: {
+    directory: {
+      path: "./public",
+      listing: false,
+      index: false
     }
+  }
 });
 
 server.route({
-    path: "/",
-    method: "GET",
-    handler: function(request, reply) {
-        reply("Hello");
+  path: "/",
+  method: "GET",
+  handler: function(request, reply) {
+    reply("Hello");
+  }
+});
+
+
+server.route({
+  path: "/api/all",
+  method: "GET",
+  handler: function list(request, reply) {
+    var item = require('./db/models/item.js');
+    item.findAll(function (err, result) {
+      if (err) {
+        reply({error: "There was an error getting all the hackers."});
+      }
+      else {
+        reply(result);
+      }
+    })
+  }
+});
+
+server.route({
+  path: "/api/{type}",
+  method: "GET",
+  handler: function list(request, reply) {
+    if(!request.params.type.match(alphanumExp)){
+      reply({error: "Please insert a valid type"});
     }
+    var item = require('./db/models/item.js');
+    item.number(request.params.type,function (err, result) {
+      if (err) {
+        reply({error: "There was an error getting all the hackers."});
+      }
+      else {
+        reply(result);
+      }
+    })
+  }
 });
 
-
 server.route({
-    path: "/api/{type}",
-    method: "GET",
-    handler: function list(request, reply) {
-      var item = require('./db/models/item.js');
-      item.number(request.params.type,function (err, result) {
-        if (err) {
-          reply({error: "There was an error getting all the hackers."});
-        }
-        else {
-          reply(result);
-        }
-      })
+  path: "/api/remove/{id}",
+  method: "DELETE",
+  handler: function create(request, reply) {
+    if(!request.params.id.match(alphanumExp)){
+      reply({error: "Please insert a valid id"});
+      return;
     }
-});
+    var item = require('./db/models/item.js');
+
+    item.countWithId(request.params.id,function(err,result){
+      if(err || result < 1){
+        reply({error: "Item doesn't exit"});
+      }else{
+        item.remv(request.params.id,function (err, result) {
+          if (err) {
+            reply({error: "There was an error deleting item"});
+          }
+          else {
+            reply({success: "Item removed"});
+          }
+        })
+      }
+    })
+  }
+
+})
 
 server.route({
-    path: "/api/{type}/{number}",
-    method: "POST",
-    handler: function create(request, reply) {
+  path: "/api/{type}/{number}",
+  method: "POST",
+  handler: function create(request, reply) {
 
+    if(!request.params.type.match(alphanumExp)){
+      reply({error: "Please insert a valid type"});
+    }
     var itemModel = require('./db/models/item.js');
-   for(var i = 0; i <request.params.number; i++){
+    if( !isNaN(request.params.number)){
+      reply({error: "Please insert a number of "+request.params.type+" to add"});
+      return;
+    }
+    for(var i = 0; i <request.params.number; i++){
       var item = new itemModel(request.payload);
       item.timestamp = Date.now();
       item.type = request.params.type; 
@@ -64,7 +119,7 @@ server.route({
           reply({success: "item created."});
         }
       });
-      }
     }
+  }
 });
 
